f65f01189793c898ef47bfdcc0fe2a567f97a499
Chon <48922526+Krystalblast@users.noreply.github.com>
2019-03-25 14:29:43 -0400

    Create PrintASCII.c

---
+++ lab2/ex3/PrintASCII.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/*Chonsuta
+*CIS2010
+*2/4/2019
+*EX3 Lab2
+* function.c to dispaly the ASCII values
+*/
+void PrintASCII(char string[])
+{
+	for(int i=0; i<=string[i];i++)
+	{
+		printf("%c ",string[i]);
+	}
+	printf("\n");
+
+	for(int i=0; i<=string[i];i++)
+	{
+		printf("%x ",string[i]);
+	}
+	printf("\n");
+
+	for(int i=0; i<=string[i]; i++)
+	{
+		printf("%d ",string[i]);
+	}
+	printf("\n");
+}
